5341ef518efc4d0225501a7ef9d4adb695f8aa37
Abel Akmal <abelakmal06@gmail.com>
2023-05-12 18:45:54 +0700

    membuat project spring

---
+++ src/test/java/com/abel/tokoonline/TokoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abel.tokoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
